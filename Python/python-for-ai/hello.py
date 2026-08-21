8067ec278ced53b91adecd924b828f2a3079768a
veerpast <131589298+veerpast@users.noreply.github.com>
2026-08-22 01:02:06 +0530

    fix: correct interpreter paths to use ${workspaceFolder}, move notebook to proper location, clean __pycache__

--- Python/python-for-ai/hello.py
+++ Python/python-for-ai/hello.py
@@ -1,3 +1,12 @@
-import requests
-response = requests.get('https://api.github.com')
-print(response.status_code)
+print("Hello, World!")
+total = 10 - 6
+print(total)
+
+power = 10 ** 4
+print(power)
+
+string = 'hello guys'
+
+long_string = '''this
+is along string'''
+print(long_string)
